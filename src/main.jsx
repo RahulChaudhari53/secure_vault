30207c4343f16a27578ac2fa9b7c9de537706e6e
Rahul Chaudhari <chaudhariraul5@gmail.com>
2026-02-05 08:38:33 +0545

    Add main entry point and global styles

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
